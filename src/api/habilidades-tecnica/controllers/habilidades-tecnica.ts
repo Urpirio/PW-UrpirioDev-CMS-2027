ad709d606d6796b183d5d359e6d8d19eb778d998
urpirio <urpiriojunior@gmail.com>
2026-07-28 11:28:11 -0400

    feat: agregar módulo habilidades-tecnica

---
+++ src/api/habilidades-tecnica/controllers/habilidades-tecnica.ts
@@ -0,0 +1,7 @@
+/**
+ * habilidades-tecnica controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::habilidades-tecnica.habilidades-tecnica');
---
+++ src/api/habilidades-tecnica/services/habilidades-tecnica.ts
@@ -0,0 +1,7 @@
+/**
+ * habilidades-tecnica service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::habilidades-tecnica.habilidades-tecnica');
